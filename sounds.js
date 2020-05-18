2f6e6d970ca89f101bd363468eea9ffd67db02e6
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-05-18 15:34:00 +0200

    invert filter

---
+++ sounds.js
@@ -0,0 +1,35 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,
+    1,
+	9,
+    10,
+	11.5,
+	15,
+    16.2,
+    17,
+	25.4,
+	26.2, //10
+    27.0,
+	29.5,
+	31.5,
+	34,
+    40,
+    44,
+    48,
+    50,
+    52,
+    54, //20
+    56,
+    58,
+    60,
+    62,
+    64, //25
+    66,
+    68,
+    70,
+    72,
+    75,//30
+	86 
+];
